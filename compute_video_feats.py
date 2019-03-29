32eab9738b9a67464460e5203ef61a946df7378e
vinace <vinsent.storage@gmail.com>
2019-03-29 18:12:05 +0530

    stopping debugger

--- compute_video_feats.py
+++ compute_video_feats.py
@@ -167,7 +167,7 @@ def preprocess_frame_full(I, aencoder, resize):
 
     I = I.astype('float32') / 255.0
     # I = torch.from_numpy(I.transpose([2, 0, 1])).cuda()
-    pdb.set_trace()
+    # pdb.set_trace()
     I = torch.from_numpy(I.transpose([2, 0, 1]))
     I = Variable(preprocess(I), volatile=True).cuda()
     fc = aencoder(I, resize)
